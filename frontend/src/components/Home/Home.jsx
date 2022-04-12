52f9eae8591bd43b1a99941780bc2e5b8ab65280
jflad17 <56899029+jflad17@users.noreply.github.com>
2022-04-11 20:09:55 -0500

    Added Home Page

---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// import Page from '../Page/Page';
+
+/**
+ * Home Page Component
+ * @returns {JSX}
+ */
+
+const Home = () => {
+  return (
+    <>
+      {/* <Page>*/}
+      <h1>Welcome Home</h1>
+      {/* </Page>*/}
+    </>
+  );
+};
+
+export default Home;
